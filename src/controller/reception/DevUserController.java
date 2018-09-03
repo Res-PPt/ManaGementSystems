d18e46a588469b439209019c41b2b9a7042e3246
ruier1 <948331956@qq.com>
2018-09-03 21:09:07 +0800

    前后台分明控制李泽军提交

--- src/controller/reception/DevUserController.java
+++ src/controller/reception/DevUserController.java
@@ -1,9 +1,9 @@
-package controller.backstage;
+package controller.reception;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 /**
- * 开发者
+ * 开发者后台
  * @author Administrator
  *
  */
